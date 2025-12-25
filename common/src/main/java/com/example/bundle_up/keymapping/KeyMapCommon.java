6d594613e0d17b90d9dd4a83e77b3e2e0c01dd37
yuri-almir-pinto <yurialmirp@gmail.com>
2025-12-25 17:35:54 -0300

    Added localization

--- common/src/main/java/com/example/bundle_up/keymapping/KeyMapCommon.java
+++ common/src/main/java/com/example/bundle_up/keymapping/KeyMapCommon.java
@@ -3,10 +3,13 @@ package com.example.bundle_up.keymapping;
 import org.lwjgl.glfw.GLFW;
 
 public class KeyMapCommon {
+    public static final String KEY_ID = "key.bundle-up-bundle-item";
+    public static final String CATEGORY = "key.category.bundle-up.category-name";
+
     public static final CommonKey BUNDLE_UP = new CommonKey(
-            "Bundle",
+            KEY_ID,
             GLFW.GLFW_KEY_G,
-            "Bundle up"
+            CATEGORY
     );
 
     public record CommonKey(String name, int key, String category){}
