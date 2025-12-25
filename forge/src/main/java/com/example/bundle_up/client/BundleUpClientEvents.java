8e01ea8c80c308b1c8b7c568d079b191b13eabdd
yuri-almir-pinto <yurialmirp@gmail.com>
2025-12-25 20:00:33 -0300

    Removed comments from Gemini

--- forge/src/main/java/com/example/bundle_up/client/BundleUpClientEvents.java
+++ forge/src/main/java/com/example/bundle_up/client/BundleUpClientEvents.java
@@ -12,28 +12,20 @@ import net.minecraftforge.fml.common.Mod;
 @Mod.EventBusSubscriber(modid = Constants.MOD_ID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class BundleUpClientEvents {
 
-    // 2. LISTEN FOR INPUT IN GUI (Runs on Forge Bus)
     @SubscribeEvent
     public static void onScreenInternalKeyPress(ScreenEvent.KeyPressed.Pre event) {
-        // Only care if we are in an inventory
         if (!(event.getScreen() instanceof AbstractContainerScreen<?> containerScreen)) {
             return;
         }
-
-        // Check if the pressed key matches our bound key
-        // We use matches() because consumeClick() fails in GUIs
         if (BundleUpForgeClient.BUNDLE_UP_KEY.matches(event.getKeyCode(), event.getScanCode())) {
 
             var keyPressedEvent = new InputHandler.KeyPressedEvent(containerScreen, event.getKeyCode());
 
-            // Run common logic
             boolean shouldSend = InputHandler.handleBundleUpKey(keyPressedEvent);
 
             if (shouldSend) {
-                // Send Packet using Forge Channel
                 ForgePacketHandler.INSTANCE.sendToServer(new ForgePacketHandler.BundleC2SPacket());
 
-                // Stop the key from doing anything else (like typing in the search bar)
                 event.setCanceled(true);
             }
         }
